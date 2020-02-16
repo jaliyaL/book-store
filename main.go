b515e66e7837fb1725282023614f8789492bf33e
Jaliya Lamahewa <jaliya@pickme.lk>
2020-02-16 21:55:53 +0530

    connect mysql db

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -17,6 +19,21 @@ type Book struct {
 var books []Book
 
 func main() {
+
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err := sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/test")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
+
 	fmt.Println("Hello world")
 
 	r := mux.NewRouter()
