b0ff6caf8df977532c68981d139676348e4b55f6
Jaliya Lamahewa <jaliya@pickme.lk>
2020-02-27 08:47:32 +0530

    connect to db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
@@ -18,15 +19,33 @@ type Book struct {
 
 var books []Book
 
+//var db *sql.DB
+var err error
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPass := "example"
+	dbName := "book-store"
+	dbHost := "172.18.0.3:3306"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+")/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 func main() {
 
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/book-store")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Connection Established")
-	}
-	defer db.Close()
+	//db, err := sql.Open("mysql", "root:example@tcp(172.18.0.2:3306)/book-store")
+	//
+	//err = db.Ping()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//
+	//defer db.Close()
 
 	fmt.Println("Hello world")
 
@@ -37,7 +56,7 @@ func main() {
 	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":1234", r))
+	log.Fatal(http.ListenAndServe(":2213", r))
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
@@ -45,12 +64,45 @@ func getBooks(w http.ResponseWriter, r *http.Request) {
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	//db, err := sql.Open("mysql", "root:example@tcp(172.18.0.2:3306)/book-store")
+	//
+	//err = db.Ping()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//
+	//defer db.Close()
+
 	fmt.Println("getBook")
 
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	fmt.Fprintf(w, "You've requested book id: %s\n", id)
+	log.Println("id", id)
+
+	var book Book
+
+	query := "select * from books where id = ?"
+
+	rows := db.QueryRow(query, id)
+
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	if err != nil {
+		log.Fatal(err, err.Error())
+	}
+	////fmt.Println(book)
+	//fmt.Println("bi")
+	//err = db.QueryRow("select * from books where id=?", id).Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	//fmt.Println("hi")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(book)
+	return
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
