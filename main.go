b0876803e05ce4f6a0fbd5f1001d7d40d36ee0e1
Jaliya Lamahewa <jaliya@pickme.lk>
2020-05-09 14:07:12 +0530

    add consumer

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ import (
 	logger "github.com/sirupsen/logrus"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
 )
 
@@ -69,9 +71,66 @@ func main() {
 	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
 
+	//r.HandleFunc("/consume", consume).Methods("GET")
+
 	r.Handle("/metrics", promhttp.Handler())
 
 	log.Fatal(http.ListenAndServe(":2213", r))
+}
+
+func consume() {
+
+	fmt.Println("consume")
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+
+	// Specify brokers address. This is default one
+	brokers := []string{"localhost:9092"}
+
+	// Create new consumer
+	master, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := master.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	topic := "book-topic"
+	// How to decide partition, is it fixed value...?
+	consumer, err := master.ConsumePartition(topic, 0, sarama.OffsetOldest)
+	if err != nil {
+		panic(err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+
+	// Count how many message processed
+	msgCount := 0
+
+	// Get signnal for finish
+	doneCh := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case err := <-consumer.Errors():
+				fmt.Println(err)
+			case msg := <-consumer.Messages():
+				msgCount++
+				fmt.Println("Received messages", string(msg.Key), string(msg.Value), msg.Headers)
+			case <-signals:
+				fmt.Println("Interrupt is detected")
+				doneCh <- struct{}{}
+			}
+		}
+	}()
+
+	<-doneCh
+	fmt.Println("Processed", msgCount, "messages")
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
